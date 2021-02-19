f6c7d8f6ad4f76c13570802340de82e368986075
Andy Strube <andystrube64@gmail.com>
2021-02-18 17:02:37 -0800

    displaying data

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+posts = [
+  {
+    'author': 'andy',
+    'title': 'title_1',
+    'content': 'content 1',
+    'date_posted': '2/18/2020'
+  },
+    {
+    'author': 'andy',
+    'title': 'title_2',
+    'content': 'content 2',
+    'date_posted': '2/18/2021'
+  }
+]
+
 def home(request):
-  return render(request, 'blog/home.html')
+  return render(request, 'blog/home.html', { 'posts': posts })
 
 def about(request):
-  return render(request, 'blog/about.html')
+  return render(request, 'blog/about.html', {'title': 'About'})
