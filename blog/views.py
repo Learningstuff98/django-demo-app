67a8ec567760d487c4c390020d532b2f535a554f
Andy Strube <andystrube64@gmail.com>
2021-02-19 20:06:48 -0800

    Added post model

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
-
-posts = [
-  {
-    'author': 'andy',
-    'title': 'title_1',
-    'content': 'content 1',
-    'date_posted': '2/18/2020'
-  },
-    {
-    'author': 'andy',
-    'title': 'title_2',
-    'content': 'content 2',
-    'date_posted': '2/18/2021'
-  }
-]
+from .models import Post
 
 def home(request):
-  return render(request, 'blog/home.html', { 'posts': posts })
+  return render(request, 'blog/home.html', { 'posts': Post.objects.all() })
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
