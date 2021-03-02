dfaec67dd1124758ef64103e5120f285c67708bd
Andy Strube <andystrube64@gmail.com>
2021-03-02 14:42:52 -0800

    Added class based view for the homepage

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 def home(request):
   return render(request, 'blog/home.html', { 'posts': Post.objects.all() })
+
+class PostListView(ListView):
+  model = Post
+  template_name = 'blog/home.html'
+  context_object_name = 'posts'
+  ordering = ['date_posted']
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
