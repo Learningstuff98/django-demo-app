f6833aca5dba450b922b64b379b3ef5f41e00ac0
Andy Strube <andystrube64@gmail.com>
2021-03-02 16:57:59 -0800

    Users can make posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+  ListView,
+  DetailView,
+  CreateView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 def home(request):
@@ -13,6 +18,14 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
   model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
