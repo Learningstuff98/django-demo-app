bbdd59ae87bb1ebde1ab953836e3e909b60ebea6
Andy Strube <andystrube64@gmail.com>
2021-03-02 18:56:29 -0800

    posts can be deleted

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.views.generic import (
   ListView,
   DetailView,
   CreateView,
-  UpdateView
+  UpdateView,
+  DeleteView
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
@@ -38,6 +39,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
   def form_valid(self, form):
     form.instance.author = self.request.user
     return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+  model = Post
+  success_url = '/'
 
   def test_func(self):
     post = self.get_object()
